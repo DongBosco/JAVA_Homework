d078473ecf2ac1856e1bca1eb914a4581ef61faf
kiwoom <dk960227@hanmail.net>
2023-09-14 01:00:04 +0900

    Homework_2_first_commit_20230914

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    private static void run(){
+        SharedObject a = new SharedObject();
+    }
+    public static void main(String[] args) {
+        run();
+    }
+}
